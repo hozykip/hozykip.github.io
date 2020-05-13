043a38ac388c3dc41c46687fd4fd425a369e60dd
kipruto hosea <kiprutohosea05@gmail.com>
2020-05-13 18:18:21 +0300

    skills

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -36,6 +36,287 @@ export default function AboutPage() {
             </p>
           </article>
         </div>
+
+        <div className="skills_container">
+          <h2>Skills</h2>
+
+          <div className="skills">
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#55b36e",
+                        strokeDashoffset: "calc(310 - (310 * 85) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      85<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">HTML5 & CSS</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#1881faad",
+                        strokeDashoffset: "calc(310 - (310 * 90) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      90<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Core PHP</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#4c6e94",
+                        strokeDashoffset: "calc(310 - (310 * 80) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      80<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">JavaScript</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#8c5865",
+                        strokeDashoffset: "calc(310 - (310 * 90) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      90<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Symfony</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#6d6d03",
+                        strokeDashoffset: "calc(310 - (310 * 80) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      80<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">React</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#105652",
+                        strokeDashoffset: "calc(310 - (310 * 90) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      90<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Java SE</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#edc2efb0",
+                        strokeDashoffset: "calc(310 - (310 * 90) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      90<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">JQUERY</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#d45e0787",
+                        strokeDashoffset: "calc(310 - (310 * 80) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      80<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Database administration</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#712b9ecc",
+                        strokeDashoffset: "calc(310 - (310 * 75) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      75<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Wordpress</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#ff253a85",
+                        strokeDashoffset: "calc(310 - (310 * 75) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      75<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Android</h2>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="box">
+                <div className="percent">
+                  <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle
+                      cx="70"
+                      cy="70"
+                      r="50"
+                      style={{
+                        stroke: "#57062ccc",
+                        strokeDashoffset: "calc(310 - (310 * 70) / 100)",
+                      }}
+                    />
+                  </svg>
+                  <div className="number">
+                    <h2 className="num_percentage">
+                      70<span>%</span>
+                    </h2>
+                  </div>
+                </div>
+                <h2 className="text">Linux server administration</h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </section>
   );

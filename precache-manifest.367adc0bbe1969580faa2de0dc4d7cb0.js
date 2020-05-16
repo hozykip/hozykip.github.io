76dfcc175ca70c26b9da665ffe781e8ebed86c17
kipruto hosea <kiprutohosea05@gmail.com>
2020-05-16 10:07:01 +0300

    Updates

---
+++ precache-manifest.367adc0bbe1969580faa2de0dc4d7cb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5c1665bfe716c0883c8274e4548fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1262599b6e060f9ac3d1",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9ac40a1b98a8284cf84f",
+    "url": "/static/css/main.5dfc1cde.chunk.css"
+  },
+  {
+    "revision": "1262599b6e060f9ac3d1",
+    "url": "/static/js/2.5e017e37.chunk.js"
+  },
+  {
+    "revision": "9df086e019b6760b6a18a830a9d39042",
+    "url": "/static/js/2.5e017e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac40a1b98a8284cf84f",
+    "url": "/static/js/main.8d6a0e60.chunk.js"
+  },
+  {
+    "revision": "1d7cdd45563d99e6e2ec",
+    "url": "/static/js/runtime-main.537fce44.js"
+  }
+]);

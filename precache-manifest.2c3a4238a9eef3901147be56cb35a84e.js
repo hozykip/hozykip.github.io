f1cd8a3df2912c5d0390d67c0324acef1812424b
hosea kipruto <hosea.cherop@agilebiz.co.ke>
2021-06-23 22:39:43 +0300

    Updates

---
+++ precache-manifest.2c3a4238a9eef3901147be56cb35a84e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597cb790567f064cd1a9dc3008f9f96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd7b26c2966ef6aedfb2",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "82bcdb1638e32967dbab",
+    "url": "/static/css/main.c5c2c8b4.chunk.css"
+  },
+  {
+    "revision": "fd7b26c2966ef6aedfb2",
+    "url": "/static/js/2.22ccedc5.chunk.js"
+  },
+  {
+    "revision": "9df086e019b6760b6a18a830a9d39042",
+    "url": "/static/js/2.22ccedc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bcdb1638e32967dbab",
+    "url": "/static/js/main.7ad82c92.chunk.js"
+  },
+  {
+    "revision": "1d7cdd45563d99e6e2ec",
+    "url": "/static/js/runtime-main.537fce44.js"
+  }
+]);
